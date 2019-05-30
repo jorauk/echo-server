3c0876f7a910fdcaab1b66fc75572dda779a4fef
Jonathan Mauk <jorauk@uw.edu>
2019-05-30 14:39:35 -0700

    A couple small variable use/formatting changes to try to fix a bug.

--- echo_server.py
+++ echo_server.py
@@ -9,7 +9,7 @@ def server(log_buffer=sys.stderr):
     
     # Instantiate a TCP socket with IPv4 Addressing.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
-        socket.IPPROTO_TCP)
+                         socket.IPPROTO_TCP)
     
     # Socket setting to avoid 'port in use' error.
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,9 +18,9 @@ def server(log_buffer=sys.stderr):
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
     # Bind 'sock' to address above and begin to listen
-    #   for incoming connections
+    # for incoming connections
     sock.bind(address)
-    sock.listen(1)
+    sock.listen()
     
     try:
         # The outer loop controls the creation of new connection sockets. The
@@ -42,7 +42,7 @@ def server(log_buffer=sys.stderr):
                     print('received "{0}"'.format(data.decode('utf8')))
                     
                     # Send the data you received back to the client.
-                    conn.sendall(data)
+                    conn.send(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
                     
                     # Check here to see whether you have received the end
@@ -54,14 +54,14 @@ def server(log_buffer=sys.stderr):
                 traceback.print_exc()
                 sys.exit(1)
             finally:
-                #       Close the socket created above.
+                # Close the socket created above.
                 conn.close()
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
 
     except KeyboardInterrupt:
-        #       Close the server socket and exit from the server function.
+        # Close the server socket and exit from the server function.
         sock.close()
         print('quitting echo server', file=log_buffer)
 
