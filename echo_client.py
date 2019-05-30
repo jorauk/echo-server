6eb99cf77b8a3c3835d4a2430ddf363bd26e391d
Jonathan Mauk <jorauk@uw.edu>
2019-05-30 14:20:44 -0700

    Code to connect to connect socket to server. I had entered the values manually before I looked up and remembered that server_address was there.

--- echo_client.py
+++ echo_client.py
@@ -5,13 +5,14 @@ import traceback
 
 def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
+    
+    # Instantiate a TCP socket with IPv4 Addressing.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
         socket.IPPROTO_TCP)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
-    # TODO: connect your socket to the server here.
-
+    
+    # Connect your socket to the server here.
+    sock = sock.connect(server_address)
     # you can use this variable to accumulate the entire message received back
     # from the server
     received_message = ''
